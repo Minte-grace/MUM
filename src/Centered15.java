5467e0c68e79781e501e02d09da8152c128eb2cb
Minte-grace <mintesnottsegaye23@gmail.com>
2022-11-11 17:42:13 +0300

    is centered updated

--- src/Centered15.java
+++ src/Centered15.java
@@ -1,8 +1,7 @@
-import java.awt.*;
-
 public class Centered15 {
     public static void main(String[] args){
         System.out.println(isCentered15(new int[]{3, 2, 10, 4, 1, 6, 9}));
+        System.out.println(isCentered15(new int[]{3, 2, 10, 5, 6, 9}));
         System.out.println(isCentered15(new int[]{2, 10, 4, 1, 6, 9}));
         System.out.println(isCentered15(new int[]{3, 2, 10, 4, 1, 6}));
         System.out.println(isCentered15(new int[]{1,1,8, 3, 1, 1}));
@@ -10,28 +9,19 @@ public class Centered15 {
         System.out.println(isCentered15(new int[]{1, 1, 2, 2, 1, 1}));
         System.out.println(isCentered15(new int[]{1, 1, 15, -1,-1}));
     }
-    static int isCentered15(int[] a){
-        boolean evenLength = a.length/2 == 0 ? true : false;
-        int startingIndex = 0;
-        int endIndex = 0;
+    static int isCentered15(int[] a) {
+        boolean isEven = a.length % 2 == 0 ? true : false;
+        int startingIndex = isEven ? a.length / 2 - 1: a.length / 2;
+        int endIndex = a.length / 2;
 
-        if(a.length <= 1) return 0;
-        if(evenLength){
-            startingIndex = a.length/2-1;
-            endIndex = a.length/2;
-        } else {
-            startingIndex = a.length/2;
-            endIndex = a.length/2;
-        }
-
-        while(startingIndex>=0 && endIndex < a.length){
+        while (startingIndex >= 0 && endIndex < a.length) {
             int sum = 0;
-            for(int i= startingIndex; i<=endIndex; i++){
-                sum = sum+ a[i];
+            for (int i = startingIndex; i <= endIndex; i++) {
+                sum = sum + a[i];
             }
-            if(sum == 15 && startingIndex == a.length - 1 - endIndex){
+            if (sum == 15 && startingIndex == a.length - endIndex - 1 && startingIndex != 0) {
                 return 1;
-           }
+            }
             startingIndex--;
             endIndex++;
         }
