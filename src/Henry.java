b47806a04c2d14c5bf420a9332a3addf6f6bd985
Minte-grace <mintesnottsegaye23@gmail.com>
2022-11-11 19:50:47 +0300

    henry number

--- src/Henry.java
+++ src/Henry.java
@@ -1,17 +1,29 @@
-//public class Henry {
-//    public static void main(String[] args){
-//
-//    }
-//    static int henry (int i, int j){
-//        int maximumIteration = i>j ? i:j;
-//        int[] arrayOfPerfectNumbers = new int[maximumIteration];
-//        int maximumInteger = Integer.MAX_VALUE;
-//
-//        int x =0;
-//        while(x < maximumInteger){
-//
-//        }
-//
-//
-//    }
-//}
+public class Henry {
+    public static void main(String[] args){
+        System.out.println(henry(1,3));
+    }
+    static int henry (int i, int j){
+       int maximumValue = Integer.MAX_VALUE;
+       int arrayLength = i>j? i:j;
+       int[] arrayOfPerfectNumbers = new int[arrayLength];
+       int index = 0;
+       int y = 1;
+       while (y<maximumValue){
+           int sum = 0;
+           for(int x = 1; x<y; x++){
+               if(y%x == 0){
+                   sum = sum+ x;
+               }
+           }
+           if(sum == y){
+               arrayOfPerfectNumbers[index] = y;
+               index++;
+           }
+           if(index==arrayLength){
+               break;
+           }
+           y++;
+       }
+       return arrayOfPerfectNumbers[i-1]+arrayOfPerfectNumbers[j-1];
+    }
+}
