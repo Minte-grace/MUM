7f8c94ada3155482d9308227c03e796d7e21de0b
Minte-grace <mintesnottsegaye23@gmail.com>
2022-11-13 12:26:45 +0300

    is legal number

--- src/IsSquare.java
+++ src/IsSquare.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class IsSquare {
+public class IsSquare {
+    public static void main(String[] arg){
+        System.out.println(isSquare(4));
+        System.out.println(isSquare(25));
+        System.out.println(isSquare(-4));
+        System.out.println(isSquare(8));
+        System.out.println(isSquare(0));
+    }
+    static int isSquare(int n){
+        for (int i=0; i<=n; i++){
+            if(i*i == n){
+                return 1;
+            }
+        }
+        return 0;
+    }
 }
