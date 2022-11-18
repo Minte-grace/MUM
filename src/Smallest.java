757306d956fc11cd9f936fe38550b11c7544e9b0
Minte-grace <mintesnottsegaye23@gmail.com>
2022-11-18 17:00:19 +0300

    s smallest

--- src/Smallest.java
+++ src/Smallest.java
@@ -1,35 +1,35 @@
 public class Smallest {
     public static void main(String[] args){
         System.out.println(smallest(4));
-//        System.out.println(smallest(3));
+        System.out.println(smallest(5));
+        System.out.println(smallest(6));
+        System.out.println(smallest(7));
+        System.out.println(smallest(3));
+
     }
     static int smallest(int n){
-        int i=1;
         int maxValue = Integer.MAX_VALUE;
-        int count = 0;
 
-        while(i<maxValue){
+        for(int i=1; i<maxValue; i++){
+            int countOccurence = 0;
             for(int j=1; j<=n; j++){
-                int numberMultiplied = i*j;
-                boolean isTwoFound = false;
-                while (numberMultiplied>0){
-                    int currentDigit = numberMultiplied%10;
-                    if(currentDigit == 2) {
-                        isTwoFound = true;
+                int testedNumber = i*j;
+                boolean isTwoExist = false;
+                while (testedNumber>0){
+                    if(testedNumber%10 ==2){
+                        isTwoExist = true;
                         break;
                     }
-                    numberMultiplied/=10;
+                    testedNumber/=10;
                 }
-                if(isTwoFound){
-                    count++;
-//                    continue;
+                if(isTwoExist){
+                    countOccurence++;
+                    continue;
                 } else {
                     break;
                 }
             }
-//            System.out.println(count);
-            if(count == n) return i;
-            i++;
+            if(countOccurence == n) return i;
         }
         return 0;
     }
